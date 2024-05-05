7d78de443425583805980b2d4d17d481c2bfd75b
Alejandro Mejías <ale.mejh@gmail.com>
2024-05-05 16:28:25 -0400

    Actualizado hasta el ejercicio08 de while

---
+++ ejercicio08.py
@@ -0,0 +1,18 @@
+'''
+1.	Pida números positivos al usuario hasta que ingrese un número negativo. El programa debe imprimir 
+una lista con todos los números ingresados y la suma de todos los números que ingresó (incluyendo el 
+número negativo).
+'''
+
+lista_numeros = []
+numero = 0
+suma = 0
+print('Este programa calculará suma de los números que usted ingrese, siempre que sean mayores o iguales a cero.\nCuando ingrese un número negativo, el programa terminará de ejecutarse.')
+
+while numero >= 0:
+    numero = float(input('Ingrese un número: '))
+    if (numero >=0):
+        lista_numeros.append(numero)
+        suma = suma + numero
+
+print(f'La lista de números es {lista_numeros}.\nLa suma de los números es de {suma}')
